2ad522164725aafe7c271b7a8858771fe3f9bb06
Stephen Baugh <steve@baugh.org.uk>
2020-04-01 09:39:51 +0100

    Docstring for class Polar modified

--- graphqt5.py
+++ graphqt5.py
@@ -4,7 +4,7 @@
 
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -712,9 +712,7 @@ class Poly(Graph):
 
 class Polar(Graph):
 
-    """A graph object for use with Python Qt based programs. Draws the
-        graph lines as poly lines to improve performance over the Graph
-        object, but with reduced draw options.
+    """A graph object for use with Python Qt based programs.
 
         Useage:
 
